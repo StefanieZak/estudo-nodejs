daefd2949f2578b589edae2660a7662606ca7e3c
StefanieZak <stefaniezak@hotmail.com>
2021-10-07 21:53:10 -0300

    Created project's base

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello there!! :D</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style scoped></style>
