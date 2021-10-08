63457b2fde0673a05d06099736d6f1994ee82cd3
StefanieZak <stefaniezak@hotmail.com>
2021-10-08 16:22:14 -0300

    created front-end

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { v4 as uuid } from "uuid";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "*" })); // origin define o endereço que poderá fazer as requisições
 
 interface User {
   id: string;
